58a411f4250aa06e90efbde7b44f69e95c328e1d
Learningitnow <70935742+Learningitnow@users.noreply.github.com>
2020-11-19 22:33:59 -0500

    added serverwork and manifest

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 const app = express();
 
@@ -24,7 +24,6 @@ mongoose.connect(
     useFindAndModify: false
   }
 );
-
 
 // routes
 app.use(require("./routes/api.js"));
